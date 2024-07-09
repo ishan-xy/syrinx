640bb84c815d3057f290553c488e1a8fdd2f6ac1
Shreyas Mahajan <shreyasmahajan05@gmail.com>
2024-07-10 04:35:09 +0530

    broken interaction

---
+++ questions/Questions.gd
@@ -0,0 +1,46 @@
+extends CanvasLayer
+
+@export_file("*.json") var d_file
+var dialogue = []
+var current_dialogue_id=0
+var d_active=false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$NinePatchRect.visible=false
+	d_file = "res://questions/json/questions.json"
+
+func start():
+	if d_active:
+		return
+	d_active=true
+	$NinePatchRect.visible=true
+	dialogue = load_dialogue()
+	current_dialogue_id=-1
+	next_script()
+	
+func load_dialogue():
+	var file = FileAccess.open(d_file, FileAccess.READ)
+	if file.file_exists(d_file):
+		var content = file.get_as_text()
+		return JSON.parse_string(content)
+
+func _input(event):
+	if not d_active:
+		return
+	if event.is_action_pressed("ui_accept"):
+		next_script()
+
+func next_script():
+	
+	current_dialogue_id+=1
+	if current_dialogue_id>=len(dialogue):
+		$Timer.start()
+		$NinePatchRect.visible=false
+		return
+	#$NinePatchRect/Label.text = dialogue[current_dialogue_id]['question']
+	#$MarginContainer/Label.text = dialogue[current_dialogue_id]['question']
+	$NinePatchRect/Label.text = dialogue[current_dialogue_id]['question']
+
+
+func _on_timer_timeout():
+	d_active=false
---
+++ questions/textbox.gd
@@ -0,0 +1,61 @@
+extends MarginContainer
+
+
+@export_file("*.json") var d_file
+var dialogue = []
+var current_dialogue_id=0
+var d_active=false
+
+const max_width=256
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$NinePatchRect.visible=false
+	d_file = "res://questions/json/questions.json"
+
+func start():
+	if d_active:
+		return
+	d_active=true
+	$NinePatchRect.visible=true
+	dialogue = load_dialogue()
+	current_dialogue_id=-1
+	next_script()
+	
+func load_dialogue():
+	var file = FileAccess.open(d_file, FileAccess.READ)
+	if file.file_exists(d_file):
+		var content = file.get_as_text()
+		return JSON.parse_string(content)
+
+func _input(event):
+	if not d_active:
+		return
+	if event.is_action_pressed("ui_accept"):
+		next_script()
+
+func next_script():
+	
+	current_dialogue_id+=1
+	if current_dialogue_id>=len(dialogue):
+		$Timer.start()
+		$NinePatchRect.visible=false
+		return
+	$MarginContainer/Label.text=dialogue[current_dialogue_id]['question']
+	#await resized
+	#custom_minimum_size.x=min(size.x,max_width)
+	#if size.x> max_width:
+		#$MarginContainer/Label.autowrap_mode=TextServer.AUTOWRAP_WORD
+		#await resized
+		#await resized
+		#custom_minimum_size.y=size.y
+	#global_position.x-=size.x/2
+	#global_position.y-=size.y+24
+	#
+	
+		
+	
+		
+
+
+func _on_timer_timeout():
+	d_active=false
