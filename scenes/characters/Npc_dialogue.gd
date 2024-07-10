1efef1c37acd91ff17bb525a2f3a54f6bfe09a68
Shreyas Mahajan <shreyasmahajan05@gmail.com>
2024-07-10 17:30:49 +0530

    working demo without barriers

---
+++ scenes/characters/Npc_dialogue.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _input(event):
+	if event.is_action_pressed("ui_accept") and len(get_overlapping_bodies())>0:
+		use_dialogue()
+
+func use_dialogue():
+	var dialogue= get_parent().get_node("Questions")
+	if dialogue:
+		dialogue.start()
