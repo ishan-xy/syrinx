cc08230435a87aac7381b0f1a171a92527e47f34
Shreyas Mahajan <shreyasmahajan05@gmail.com>
2024-07-16 15:01:03 +0530

    question box

--- questions/dialog.gd
+++ questions/dialog.gd
@@ -1,24 +1,36 @@
-extends MarginContainer
+extends Node2D
 
 
 @export_file("*.json") var d_file
 var dialogue = []
 var current_dialogue_id=0
 var d_active=false
-
-const max_width=256
+var question: TextEdit
+var answer:TextEdit
+var submit:Button
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/TextEdit.visible=false
+	question=$question
+	answer=$answer
+	submit=$submit
+	submit.visible=false
+	question.visible=false
+	answer.visible=false
+	
+
 	$NinePatchRect.visible=false
 	d_file = "res://questions/json/questions.json"
+	start()
 
 
 func start():
 	if d_active:
 		return
 	d_active=true
-	$MarginContainer/TextEdit.visible=true	
+	submit.visible=true
+	question.visible=true
+	answer.visible=true
+	
 	$NinePatchRect.visible=true
 	dialogue = load_dialogue()
 	current_dialogue_id=-1
@@ -42,21 +54,18 @@ func next_script():
 	if current_dialogue_id>=len(dialogue):
 		$Timer.start()
 		$NinePatchRect.visible=false
-		$MarginContainer/TextEdit.visible=false	
+		submit.visible=false
+		question.visible=false
+		answer.visible=false
 		return
-	$MarginContainer/Label.text=dialogue[current_dialogue_id]['question']
-	$MarginContainer/Label.visibility_layer=false
-	$MarginContainer/TextEdit.text=dialogue[current_dialogue_id]['question']
-	
-	await resized
-	custom_minimum_size.x=min(size.x,max_width)
-	if size.x> max_width:
-		$MarginContainer/Label.autowrap_mode=TextServer.AUTOWRAP_WORD
-		await resized
-		await resized
-		custom_minimum_size.y=size.y
-	
-	
-		
+	question.text=dialogue[current_dialogue_id]['question']
+
 func _on_timer_timeout():
 	d_active=false
+
+ # Replace with function body.
+
+func _on_submit_button_down():
+	var ans=$answer.text 
+# now send this ans to web socket to server to check the ans
+	print(ans)
