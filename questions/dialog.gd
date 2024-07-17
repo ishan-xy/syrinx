60f79ab5290dafdad2e000012d168e476260599f
Shreyas Mahajan <shreyasmahajan05@gmail.com>
2024-07-18 00:01:49 +0530

    tired to add question check

--- questions/dialog.gd
+++ questions/dialog.gd
@@ -8,9 +8,9 @@ var question: TextEdit
 var answer: TextEdit
 var submit: Button
 @export var websocket_url := "ws://localhost:8000"
-var socket := WebSocketPeer.new()
-#var post_url = "http://example.com/submit"
-
+#var socket := WebSocketPeer.new()
+var wsConn=WebSocketClient.new()
+var QuestionID=1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	question = $question
@@ -20,30 +20,30 @@ func _ready() -> void:
 	#submit.visible = false
 	#question.visible = false
 	#answer.visible = false
-	var err := socket.connect_to_url(websocket_url)	
-	if err!=OK:
-		print("unable to connect")
-		set_process(false)
+	#var err := socket.connect_to_url(websocket_url)	
+	#if err!=OK:
+		#print("unable to connect")
+		#set_process(false)
 	d_file = "res://questions/json/questions.json"
 	
 	
-func _process(delta: float) -> void:
-	socket.poll()
-	var state := socket.get_ready_state()
-	if state == WebSocketPeer.STATE_OPEN:
-		while socket.get_available_packet_count():
-			var packet := socket.get_packet()
-			if packet.size() > 0:
-				var reply := packet.get_string_from_utf8()
-				print(reply)
-	elif state == WebSocketPeer.STATE_CLOSING:
-		# Keep polling to achieve proper close.
-		pass
-	elif state == WebSocketPeer.STATE_CLOSED:
-		var code := socket.get_close_code()
-		var reason := socket.get_close_reason()
-		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
-		set_process(false) 
+#func _process(delta: float) -> void:
+	#socket.poll()
+	#var state := socket.get_ready_state()
+	#if state == WebSocketPeer.STATE_OPEN:
+		#while socket.get_available_packet_count():
+			#var packet := socket.get_packet()
+			#if packet.size() > 0:
+				#var reply := packet.get_string_from_utf8()
+				#print(reply)
+	#elif state == WebSocketPeer.STATE_CLOSING:
+		## Keep polling to achieve proper close.
+		#pass
+	#elif state == WebSocketPeer.STATE_CLOSED:
+		#var code := socket.get_close_code()
+		#var reason := socket.get_close_reason()
+		#print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		#set_process(false) 
 	
 func start() -> void:
 	if d_active:
@@ -53,17 +53,20 @@ func start() -> void:
 	question.visible = true
 	answer.visible = true
 	$NinePatchRect.visible = true
-	dialogue = load_dialogue()
+	dialogue = load_dialogue(QuestionID)
 	current_dialogue_id = -1
 	next_script()
 
-func load_dialogue() -> Variant:
-	var file := FileAccess.open(d_file, FileAccess.READ)
-	if file.file_exists(d_file):
-		var content := file.get_as_text()
-		return JSON.parse_string(content)
+func load_dialogue(QuestionID) -> Variant:
+	wsConn.send(QuestionID)
+	while wsConn.get_available_packet_count():
+		var packet = wsConn.get_packet()
+		question=packet.get_string_from_ascii()
+	print(question)
+	if(question):
+		return question
 	return null
-
+	
 func _input(event: InputEvent) -> void:
 	if not d_active:
 		return
@@ -91,9 +94,8 @@ func _on_submit_button_down() -> void:
 		"QuestionID": "questionId",
 		"Answer": ans
 	}
-	
 	var json_string: Variant= JSON.stringify(string)
 	#print(json_string)
-	var byte_array: Variant = json_string.to_utf8_buffer()
+	#var byte_array: Variant = json_string.to_utf8_buffer()
 	#print(byte_array)
-	socket.send(byte_array, WebSocketPeer.WRITE_MODE_BINARY)
+	wsConn.send(json_string)
